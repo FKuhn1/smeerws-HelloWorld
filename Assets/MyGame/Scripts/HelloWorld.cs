9c9f600c1c4924931637681162c65c4215a1ec11
Kuhn <fabian.kuhn@htl-salzburg.ac.at>
2019-03-06 16:52:43 +0100

    clean code

--- Assets/MyGame/Scripts/HelloWorld.cs
+++ Assets/MyGame/Scripts/HelloWorld.cs
@@ -5,27 +5,17 @@ using System;
 using UnityEngine;
 
 
-public class HelloWorld : MonoBehaviour {
+public class HelloWorld : MonoBehaviour
+{
 
 
 	// Use this for initialization
-	void Start () {
-        string hw = "Hello World (hw)";
+	void Start ()
+    {
+        string message = "Hello World";
 
-        print("(print) Hello World");
-        Debug.Log("(Debug.Log) Hello World");
-        Console.WriteLine("(Console.WriteLine) Hello World"); //Console.WriteLine does nothing inside the Unity Engine,
-                                                              //because Unity uses a custom console, only accessed 
-                                                              //through Unity Engine: Debug.Log, ...
-                                                              //Unity doesn't display the standard C# Console.
-
-        print("(print - hw) " + hw);
-        Debug.Log("(Debug.Log - hw ) " + hw);
-        Console.WriteLine("Console.WriteLine - hw" + hw);
-	}
+        print(message);
+      
+	}	
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
